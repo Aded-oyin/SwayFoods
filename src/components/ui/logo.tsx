dcb629fb693f9d76722bc74da8d367aa98711ad7
Aded-oyin <danyodele50@gmail.com>
2023-08-03 18:29:22 +0100

    first commit

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from '@components/ui/link';
+import cn from 'classnames';
+import { siteSettings } from '@settings/site-settings';
+
+const Logo: React.FC<React.AnchorHTMLAttributes<{}>> = ({
+  className,
+  href = siteSettings.logo.href,
+  ...props
+}) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'inline-flex focus:outline-none w-auto md:w-32 lg:w-72',
+        className
+      )}
+      {...props}
+    >
+      <Image
+        src={siteSettings.logo.url}
+        alt={siteSettings.logo.alt}
+        priority
+        loading="eager"
+      />
+    </Link>
+  );
+};
+
+export default Logo;
